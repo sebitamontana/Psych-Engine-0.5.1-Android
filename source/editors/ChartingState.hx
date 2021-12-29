1d611c12824fefdcd2c55b6df8d7fdfdf6b32c85
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2021-12-29 21:08:35 +0200

    Yes

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -2677,11 +2677,15 @@ class ChartingState extends MusicBeatState
 
 		if ((data != null) && (data.length > 0))
 		{
+                        #if desktop
 			_file = new FileReference();
 			_file.addEventListener(Event.COMPLETE, onSaveComplete);
 			_file.addEventListener(Event.CANCEL, onSaveCancel);
 			_file.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
 			_file.save(data.trim(), "events.json");
+                        #else
+                        openfl.system.System.setClipboard(data.trim());
+                        #end
 		}
 	}
 
