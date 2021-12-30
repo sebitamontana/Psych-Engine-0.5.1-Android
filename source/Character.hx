905a790b89a6f8e65afa4671a2d6e0b53a2d2913
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2021-12-30 15:04:27 +0200

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -119,8 +119,8 @@ class Character extends FlxSprite
 				var json:CharacterFile = cast Json.parse(rawJson);
 				#if MODS_ALLOWED
 				var modTxtToFind:String = Paths.modsTxt(json.image);
-				var txtToFind:String = Main.getDataPath() + Paths.getPath('images/' + json.image + '.txt', TEXT);
-				if(FileSystem.exists(modTxtToFind) || FileSystem.exists(txtToFind) || Assets.exists(txtToFind))
+				var txtToFind:String = Paths.getPath('images/' + json.image + '.txt', TEXT);
+				if(FileSystem.exists(modTxtToFind) || Assets.exists(txtToFind))//This leteraly fix it
 				#else
 				if(Assets.exists(Paths.getPath('images/' + json.image + '.txt', TEXT)))
 				#end
