0bf7061d9963fb731415a365bdf0b3ab2699be33
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2021-12-30 19:38:07 +0200

    Add files via upload

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -27,7 +27,7 @@ class ClientPrefs {
 	public static var scoreZoom:Bool = true;
 	public static var noReset:Bool = false;
 	public static var healthBarAlpha:Float = 1;
-	public static var controllerMode:Bool = false;
+	public static var controllerMode:Bool = #if desktop false #else true #end;
 	public static var gameplaySettings:Map<String, Dynamic> = [
 		'scrollspeed' => 1.0,
 		'songspeed' => 1.0,
