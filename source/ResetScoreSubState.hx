4038cd83a33661a36479dacd9ab6c0b680083380
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2021-12-30 15:00:23 +0200

    Update ResetScoreSubState.hx

--- source/ResetScoreSubState.hx
+++ source/ResetScoreSubState.hx
@@ -100,11 +100,7 @@ class ResetScoreSubState extends MusicBeatSubstate
 		}
 		if(controls.BACK) {
 			FlxG.sound.play(Paths.sound('cancelMenu'), 1);
-			#if desktop
 			close();
-                        #else
-                        MusicBeatState.resetState();
-                        #end
 		} else if(controls.ACCEPT) {
 			if(onYes) {
 				if(week == -1) {
