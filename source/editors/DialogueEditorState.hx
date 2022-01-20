c589bd54e6890ef51cab49c9239b845564449d7e
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-01-20 22:09:02 +0200

    Update DialogueEditorState.hx

--- source/editors/DialogueEditorState.hx
+++ source/editors/DialogueEditorState.hx
@@ -98,6 +98,10 @@ class DialogueEditorState extends MusicBeatState
 		animText.scrollFactor.set();
 		add(animText);
 		changeText();
+
+                #if android
+		addVirtualPad(FULL, A_B_C_X_Y_Z);
+                #end
 		super.create();
 	}
 
@@ -343,8 +347,8 @@ class DialogueEditorState extends MusicBeatState
 				transitioning = true;
 			}
 			var negaMult:Array<Int> = [1, -1];
-			var controlAnim:Array<Bool> = [FlxG.keys.justPressed.W, FlxG.keys.justPressed.S];
-			var controlText:Array<Bool> = [FlxG.keys.justPressed.D, FlxG.keys.justPressed.A];
+			var controlAnim:Array<Bool> = [FlxG.keys.justPressed.W#if android || _virtualpad.buttonX.justPressed #end, FlxG.keys.justPressed.S#if android || _virtualpad.buttonA.justPressed #end];
+			var controlText:Array<Bool> = [FlxG.keys.justPressed.D#if android || _virtualpad.buttonY.justPressed #end, FlxG.keys.justPressed.A#if android || _virtualpad.buttonB.justPressed #end];
 			for (i in 0...controlAnim.length) {
 				if(controlAnim[i] && character.jsonFile.animations.length > 0) {
 					curAnim += negaMult[i];
@@ -363,7 +367,7 @@ class DialogueEditorState extends MusicBeatState
 				}
 			}
 
-			if(FlxG.keys.justPressed.O) {
+			if(FlxG.keys.justPressed.O#if android || _virtualpad.buttonZ.justPressed #end) {
 				dialogueFile.dialogue.remove(dialogueFile.dialogue[curSelected]);
 				if(dialogueFile.dialogue.length < 1) //You deleted everything, dumbo!
 				{
@@ -372,7 +376,7 @@ class DialogueEditorState extends MusicBeatState
 					];
 				}
 				changeText();
-			} else if(FlxG.keys.justPressed.P) {
+			} else if(FlxG.keys.justPressed.P#if android || _virtualpad.buttonC.justPressed #end) {
 				dialogueFile.dialogue.insert(curSelected + 1, copyDefaultLine());
 				changeText(1);
 			}
@@ -506,12 +510,15 @@ class DialogueEditorState extends MusicBeatState
 		var data:String = Json.stringify(dialogueFile, "\t");
 		if (data.length > 0)
 		{
-
+                        #if android
+                        openfl.system.System.setClipboard(data.trim());
+			#else
 			_file = new FileReference();
 			_file.addEventListener(Event.COMPLETE, onSaveComplete);
 			_file.addEventListener(Event.CANCEL, onSaveCancel);
 			_file.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
 			_file.save(data, "dialogue.json");
+                        #end
 		}
 	}
 
